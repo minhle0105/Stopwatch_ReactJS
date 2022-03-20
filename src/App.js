2509896e9fa2cf7fc18d3771503bad33de4cc6b4
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-20 19:33:20 -0400

    Split Clock component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
-import { useState, useRef } from 'react';
-
+import { Clock } from './components/Clock';
 function App() {
-  const [sec, setSec] = useState(0);
-  const [milisec, setMilisec] = useState(0);
-  const [isOn, setIsOn] = useState(false);
-
-  let intervalId = useRef(1);
-  let intervalId2 = useRef(2);
-
-  const handleStart = () => {
-    intervalId.current = setInterval(() => {
-      setSec(prevState => prevState + 1);
-    }, 1000)
-    intervalId2.current = setInterval(() => {
-      setMilisec(prevState => prevState === 1000 ? 0 : prevState + 1);
-    }, 1);
-    setIsOn(true);
-  }
-
-  const handleStop = () => {
-    setIsOn(false);
-    clearInterval(intervalId.current);
-    clearInterval(intervalId2.current);
-  }
-
-  const handleReset = () => {
-    clearInterval(intervalId.current);
-    clearInterval(intervalId2.current);
-    setSec(0);
-    setMilisec(0);
-    setIsOn(false);
-  }
-
   return (
     <div className="App">
-      <div>
-        <h1>{sec < 10 ? "0" + sec : sec} : {milisec < 10 ? '0' + (milisec + "").substring(0,2) : (milisec + "").substring(0,2)}</h1>
-        <button onClick={isOn ? handleStop : handleStart}>{isOn ? 'Stop' : 'Start'}</button>
-        <button onClick={handleReset}>Reset</button>
-      </div>
+      <Clock />
     </div>
   );
 }
---
+++ src/components/Clock.js
@@ -0,0 +1,49 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const Clock = () => {
+    const [sec, setSec] = useState(0);
+    const [milisec, setMilisec] = useState(0);
+    const [secDisplay, setSecDisplay] = useState("0" + sec)
+    const [milisecDisplay, setMiliSecDisplay] = useState("0" + milisec)
+    const [isOn, setIsOn] = useState(false);
+  
+    let intervalId = useRef(1);
+    let intervalId2 = useRef(2);
+  
+    useEffect(() => {
+      setSecDisplay(sec < 10 ? "0" + sec : sec);
+      setMiliSecDisplay(milisec < 10 ? '0' + (milisec + "").substring(0,2) : (milisec + "").substring(0,2));
+    }, [sec, milisec])
+  
+    const handleStart = () => {
+      intervalId.current = setInterval(() => {
+        setSec(prevState => prevState + 1);
+      }, 1000)
+      intervalId2.current = setInterval(() => {
+        setMilisec(prevState => prevState === 100 ? 0 : (prevState + 1));
+      }, 0.1);
+      setIsOn(true);
+    }
+  
+    const handleStop = () => {
+      setIsOn(false);
+      clearInterval(intervalId.current);
+      clearInterval(intervalId2.current);
+    }
+  
+    const handleReset = () => {
+      clearInterval(intervalId.current);
+      clearInterval(intervalId2.current);
+      setSec(0);
+      setMilisec(0);
+      setIsOn(false);
+    }
+  
+    return (
+        <div>
+          <h1>{secDisplay} : {milisecDisplay}</h1>
+          <button onClick={isOn ? handleStop : handleStart}>{isOn ? 'Stop' : 'Start'}</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+    );
+}
