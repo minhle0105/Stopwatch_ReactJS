f90d3cc57f65f6180738016c118b3a049829b7a1
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-20 19:12:43 -0400

    Add milisec display

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef } from 'react';
 
 
 
 function App() {
-  const [time, setTime] = useState(180);
+  const [time, setTime] = useState(0);
+  const [milisec, setMilisec] = useState(0);
   const [isOn, setIsOn] = useState(false);
 
   let intervalId = useRef(1);
+  let intervalId2 = useRef(2);
 
   const handleStart = () => {
     intervalId.current = setInterval(() => {
-      setTime(prevState => prevState - 1);
+      setTime(prevState => prevState + 1);
     }, 1000)
+    intervalId2.current = setInterval(() => {
+      setMilisec(prevState => prevState === 1000 ? 0 : prevState + 1);
+    }, 1);
     setIsOn(true);
     console.log("Start -> " + intervalId.current)
+    console.log("Start milisec -> " + intervalId2.current)
   }
 
   const handleStop = () => {
     console.log("Clear -> " + intervalId.current)
+    console.log("Clear milisec -> " + intervalId2.current)
     setIsOn(false);
     clearInterval(intervalId.current);
+    clearInterval(intervalId2.current);
   }
 
   const handleReset = () => {
     clearInterval(intervalId.current);
-    setTime(180);
+    clearInterval(intervalId2.current);
+    setTime(0);
+    setMilisec(0);
     setIsOn(false);
   }
 
   return (
     <div className="App">
       <div>
-        <h1>{time}</h1>
+        <h1>{time < 10 ? "0" + time : time} : {milisec < 10 ? '0' + (milisec + "").substring(0,2) : (milisec + "").substring(0,2)}</h1>
         <button onClick={isOn ? handleStop : handleStart}>{isOn ? 'Stop' : 'Start'}</button>
         <button onClick={handleReset}>Reset</button>
       </div>
